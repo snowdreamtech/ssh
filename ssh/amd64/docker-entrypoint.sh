db1a1e11f3e7892123e200bd01b997e0005e337d
snowdream <sn0wdr1am@icloud.com>
2019-11-28 15:15:31 +0800

    Update： echo "root:$SSH_PASSWORD" | chpasswd > /dev/null 2>&1
    Signed-off-by: snowdream <sn0wdr1am@icloud.com>

--- ssh/amd64/docker-entrypoint.sh
+++ ssh/amd64/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 # change the password for root
-echo "root:$SSH_PASSWORD" | chpasswd
+echo "root:$SSH_PASSWORD" | chpasswd > /dev/null 2>&1
 
 # start sshd
 nohup /usr/sbin/sshd -D > /dev/null 2>&1 &
